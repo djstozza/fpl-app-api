7760fe768bd83085e229c6287095e4b2ae302866
Daniel Sztolcman <daniel.sztolcman@gmail.com>
2021-08-14 08:56:12 +1000

    Add auth to sidekiq routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
+require 'sidekiq/web'
+require 'sidekiq-scheduler/web'
+
 Rails.application.routes.draw do
-  require 'sidekiq/web'
-  require 'sidekiq-scheduler/web'
+  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+    ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_USERNAME"])) &
+      ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_PASSWORD"]))
+  end
   mount Sidekiq::Web => '/sidekiq'
   mount ActionCable.server => '/cable'
 
